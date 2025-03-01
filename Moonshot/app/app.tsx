2c3c04a5d64af7fb255d13ad52c9e11e038940fe
Alexis SANTOS <alexis.santos@algosup.com>
2025-03-01 13:18:47 +0100

    Revert "fixed issues"
    This reverts commit 18c1a8f2c9e79934b4522b4e7745569a87c4f5bb.

--- Moonshot/app/app.tsx
+++ Moonshot/app/app.tsx
@@ -1,7 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import LoginScreen from '@/app/loginPage';
-import HomeScreen from '@/app/homeScreen';
+import LoginScreen from '@/app/LoginPage';
+import HomeScreen from '@/app/HomeScreen';
 
 const Stack = createStackNavigator();
 
